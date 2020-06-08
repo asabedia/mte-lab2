2d443e0a60f6af2d9c0a283354289cea002ea432
akandadai <anantkandadai@gmail.com>
2020-06-08 16:31:08 -0400

    delete Objects

--- Objects/main.d
+++
@@ -1,2 +0,0 @@
-.\objects\main.o: main.c
-.\objects\main.o: Z:\run\media\anant\79d5101f-a537-4803-98f5-8897ecc8505e\uVision\ARM\ARMCC\bin\..\include\stdio.h
--- Objects/retarget.d
+++
@@ -1,6 +0,0 @@
-.\objects\retarget.o: lab2_starter_code\Retarget.c
-.\objects\retarget.o: Z:\run\media\anant\79d5101f-a537-4803-98f5-8897ecc8505e\uVision\ARM\ARMCC\bin\..\include\stdio.h
-.\objects\retarget.o: Z:\run\media\anant\79d5101f-a537-4803-98f5-8897ecc8505e\uVision\ARM\ARMCC\bin\..\include\rt_misc.h
-.\objects\retarget.o: Z:\run\media\anant\79d5101f-a537-4803-98f5-8897ecc8505e\uVision\ARM\ARMCC\bin\..\include\stddef.h
-.\objects\retarget.o: Z:\run\media\anant\79d5101f-a537-4803-98f5-8897ecc8505e\uVision\ARM\ARMCC\bin\..\include\stdint.h
-.\objects\retarget.o: lab2_starter_code\uart.h
